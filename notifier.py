ccb857d87acc70baa47f3ecd47f98c30f2c0dfee
Yuvaraj Premlal <100008497+Yuvaraj-Premlal@users.noreply.github.com>
2026-03-07 11:29:50 +0530

    Delete notifier.py

--- notifier.py
+++
@@ -1,8 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-def send_completion_email(*args, **kwargs):
-    logger.info("Email notifications disabled.")
-
-def send_failure_email(*args, **kwargs):
-    logger.info("Email notifications disabled.")
